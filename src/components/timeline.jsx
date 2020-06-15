f31747b74dc323ca9c9061a3abded7c7d98676f0
shahriar-shakil <toshahriarshakil@gmail.com>
2020-06-15 17:39:15 +0600

    deploy portfolio at gh pages

---
+++ src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Pondit <span>June 2019 - Dec 2019</span></h2>
+                        <p>I recently have joined One of the Best IT training Center at Bangladesh <a href="https://www.pondit.com/">Pondit</a> for Internship </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Juniour Software Engineer at Tappware Solutions Ltd.<span> Jan 2020 - present</span></h2>
+                        <p>Working as a React Developer on Nothi - e filing system. My jobs is to Build Reusable Components for Web App,Fetch data from API and ensure offline synchronization </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2002-2018</span></h2>
+                        <p>I have completed my higher secondary education in 2014.After that I Started BBA at IUBAT in 2015 . But 2 years later I left University And Started Learning Code. </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
